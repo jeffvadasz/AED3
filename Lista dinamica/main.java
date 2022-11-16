e80fae4c635bfa607553f9a3168ecb562dbdef85
jeffvadasz <118360520+jeffvadasz@users.noreply.github.com>
2022-11-16 11:36:01 -0300

    Create main.java

---
+++ Lista dinamica/main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	   No n1 = new No(1);
+	   No n2 = new No(2);
+	  
+	    
+	    Lista l = new Lista();
+	    l.insereNoFinal(n1);
+	    l.insereNoFinal(n2);
+	    l.imprime();
+	}
+}

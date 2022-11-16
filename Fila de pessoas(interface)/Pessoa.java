0ef64aaad1668cea4654be7affcf5ba920fd2f63
jeffvadasz <118360520+jeffvadasz@users.noreply.github.com>
2022-11-16 11:32:02 -0300

    Create Pessoa.java

---
+++ Fila de pessoas(interface)/Pessoa.java
@@ -0,0 +1,24 @@
+public class Pessoa{
+    int idade;
+    char sexo;
+    String nome;
+    
+    
+    public Pessoa(int i, char s, String n){
+        idade = i;
+        sexo = s;
+        nome = n;
+    }
+    
+    void imprime(){
+        
+        System.out.println("Nome= " + nome + " " + "Idade " + idade+ " " + "Sexo " + sexo );
+    }
+    
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security

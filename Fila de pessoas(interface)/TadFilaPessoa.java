5f33006e52ce3a8aafd3f621749a3c4bb69e5908
jeffvadasz <118360520+jeffvadasz@users.noreply.github.com>
2022-11-16 11:31:39 -0300

    Create TadFilaPessoa.java

---
+++ Fila de pessoas(interface)/TadFilaPessoa.java
@@ -0,0 +1,57 @@
+public class TadFilaPessoa{
+    
+    int inicio;
+    int fim;
+    int capacidade;
+    Pessoa dados[];
+  
+    
+    public TadFilaPessoa(){
+        inicio = -1;
+        fim = -1;
+        capacidade = 50;
+        dados = new Pessoa[50];
+    }
+    public boolean enfileira(Pessoa p){
+        if (! cheia()){
+            fim ++;
+            dados [fim] = p;
+        return true;
+        }
+    return false;    
+    }
+    
+    public Pessoa desenfileira(){
+        if(!vazia())
+           inicio ++;
+        return dados[inicio];
+    }
+    
+    public void desmembra(TadFilaPessoa idoso,TadFilaPessoa naoidoso){
+        for(int i = inicio+1; i<=fim; i++){
+            if (dados[i].idade >= 60){
+                idoso.enfileira(desenfileira());
+            }
+            else{
+                naoidoso.enfileira(desenfileira());
+            }
+        }
+    }
+    
+    public void imprime(){
+        for(int i = inicio+1; i<=fim; i++){
+           dados[i].imprime();
+        }
+    }
+    
+    public boolean cheia(){
+        return (fim == capacidade-1);
+    }
+    public boolean vazia(){
+        return (conta() == 0);
+        
+    }
+    public int conta(){
+        return (fim - inicio);
+    }
+}

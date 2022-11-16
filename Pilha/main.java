20d93b380f11c72b151ca5aaa4a9b11d97bc41c4
jeffvadasz <118360520+jeffvadasz@users.noreply.github.com>
2022-11-16 11:23:27 -0300

    Create main.java

---
+++ Pilha/main.java
@@ -0,0 +1,13 @@
+/******************************************************************************
+02 - Pilha
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		TadPilha p = new TadPilha(5);
+		
+		p.empilha(1);
+		p.empilha(2);
+		p.imprime();
+	}
+}
